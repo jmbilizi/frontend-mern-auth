42535601436d15b9c29db7cad509a99f45a110c7
Admin <janviermbilizi@gmail.com>
2020-06-09 08:44:15 -0600

    uncomment build package in gitignore to push and run the production package

---
+++ build/precache-manifest.291cd30aa3e0dab15112616f3f083213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb74952df2f1169218bd8d98abfb19ee",
+    "url": "/frontend-mern-auth/index.html"
+  },
+  {
+    "revision": "00b19fcd5ff3b3964d59",
+    "url": "/frontend-mern-auth/static/css/2.6010d4dc.chunk.css"
+  },
+  {
+    "revision": "00b19fcd5ff3b3964d59",
+    "url": "/frontend-mern-auth/static/js/2.5148e8f9.chunk.js"
+  },
+  {
+    "revision": "20e794d2bc82e066633b87e6855036b9",
+    "url": "/frontend-mern-auth/static/js/2.5148e8f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7691c99310cea37ceec8",
+    "url": "/frontend-mern-auth/static/js/main.40ecd229.chunk.js"
+  },
+  {
+    "revision": "3fcf158d318c96e2b7e6",
+    "url": "/frontend-mern-auth/static/js/runtime-main.ae57d008.js"
+  }
+]);
