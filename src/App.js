b20d7b2b4adfa6a6fbaef6116346584b44f672ea
Admin <janviermbilizi@gmail.com>
2020-06-09 08:14:55 -0600

    built a secured authentication with facebook and google login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Layout from './core/Layout';
+import { Helmet } from 'react-helmet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const head = () => (
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>MERN Stack</title>
+            <link rel="canonical" href="https://mern-stack.com" />
+        </Helmet>
+    );
+    return (
+        <Layout>
+            {head()}
+            <div className="col-md-6 offset-md-3 text-center">
+                <h1 className="p-5">React Node MongoDB Authentication Boilerplate</h1>
+                <h2>MERN STACK</h2>
+                <hr />
+                <p className="lead">
+                    MERN stack login register system with account activation, forgot password, reset password, login
+                    with facebook and google as well as private and protected routes for authenticated user and users
+                    with the role of admin.
+                </p>
+                <h4 className="p2">Bonus</h4>
+                <p className="lead">Profile update & deployment to digital ocean cloud servers</p>
+            </div>
+        </Layout>
+    );
+};
 
 export default App;
